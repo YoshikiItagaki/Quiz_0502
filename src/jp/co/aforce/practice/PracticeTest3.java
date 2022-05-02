e9a92ce0689e3037b7ab0382ac0d5ebcbbcbb820
YoshikiItagaki <y.itagaki@a-force.co.jp>
2022-05-02 13:31:47 +0900

    講義を受けての修正

--- src/jp/co/aforce/practice/PracticeTest3.java
+++ src/jp/co/aforce/practice/PracticeTest3.java
@@ -1,21 +1,23 @@
 package jp.co.aforce.practice;
 
-import java.util.HashMap;
-
 public class PracticeTest3 {
 
 	public static void main(String[] args) {
 
 		//1.
-		HashMap<String, String> employeesName = new HashMap<>();
+		/*HashMap<String, String> employeesName = new HashMap<>();
 		employeesName.put("1", "田中太郎");
 		HashMap<String, String> employeesGender = new HashMap<>();
 		employeesGender.put("1", "男性");
 		HashMap<String, String> employeesAge = new HashMap<>();
 		employeesAge.put("1", "27");
-
+		*/
+		String tanakaData[] = {"1","田中太郎","男性","27"};
 		//2.
-		System.out.println(employeesName.get("1"));
+		/*		System.out.println(employeesName.get("1"));
+		*/
+		System.out.println(tanakaData[1]);
+
 
 	}
 
